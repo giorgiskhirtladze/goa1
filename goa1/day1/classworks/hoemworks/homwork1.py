ccda8e687a251b2a8ce28fe34d8edb56b40fcf6c
giorgiskhirtladze <144174679+giorgiskhirtladze@users.noreply.github.com>
2023-10-11 13:24:31 +0400

    Add files via upload

---
+++ goa1/day1/classworks/hoemworks/homwork1.py
@@ -0,0 +1,8 @@
+user_num = int(input("enter any number : "))
+
+if user_num <10000:
+    print("GOA-ში სწავლობდი?")
+    elif user_num ==10000
+else:
+    if  user_num>1000
+    print("შემოსულიყავი GOA ში მატრიცელო ")
